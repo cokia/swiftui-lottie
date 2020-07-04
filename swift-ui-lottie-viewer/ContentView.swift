6ce1cc48eddf8aa696c73db6622e5b079ff2fc8d
cokia <hwymaster01@gmail.com>
2020-07-04 23:32:21 +0900

    Initial Commit

---
+++ swift-ui-lottie-viewer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  swift-ui-lottie-viewer
+//
+//  Created by h4nuko0n on 2020/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-lottie-viewer/swift_ui_lottie_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_lottie_viewerApp.swift
+//  swift-ui-lottie-viewer
+//
+//  Created by h4nuko0n on 2020/07/04.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_lottie_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
